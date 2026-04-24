fb0373972fdc12a0ebd63dc520ab381014e0912c
Vladimir(New) <shohisuper2015@gmail.com>
2026-04-24 08:59:01 +0300

    ничего нового

--- main1.cpp
+++ main1.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!";
+    std::cout << "Hello, World!\n";
+    system("pause");
     return 0;
 }
-#include <iostream> "and take THIS!!!"
-"idk what to do with this..."
