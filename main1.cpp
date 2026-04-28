cbd32a1eb6095cb996e9b44a5fa82d79938e0f7e
Vladimir <shohisuper2015@gmail.com>
2026-04-28 11:20:33 +0500

    nothing

--- main1.cpp
+++ main1.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!\n" << endl;  
     system("pause");
-    return 0;
+    cout << 
+     "added c_cpp_properties.json in old computer";
 }
