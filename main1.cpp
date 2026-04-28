9c7443e1fd22f8df07fe1d0bbc876202036c0356
Vladimir <shohisuper2015@gmail.com>
2026-04-28 19:46:02 +0500

    rebuilding ...

--- main1.cpp
+++ main1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main() {
-    cout << "Hello, World!\n" << endl;  
-    system("pause");
-    cout << 
-     "added c_cpp_properties.json in old computer";
+    int n;
+    cin >> n;
+    auto sumln = 0.0;
+    for (int i=1;i<=n;i++) {
+        sumln+=pow(-1,i+1)/i;
+    }
+    cout << sumln;
 }
