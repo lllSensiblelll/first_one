2fa26432c06cf1d155bef4f902498495b13771b7
Vladimir(New) <shohisuper2015@gmail.com>
2026-04-29 20:00:57 +0300

    changed to curent TASKS

--- main1.cpp
+++ main1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+
 int main() {
-    cout << "Hello, World!\n" << endl;  
+    char x;
+    std::vector<char> data;
+    int a=0;
+    while (std::cin >> x) {
+        data.push_back(x);  // добавляем очередное число в вектор
+        a++;
+    }
+    std::cout << data.size() << std::endl;  // выводим размер вектора
+    for (char x : data) {
+        std::cout << x << " ";
+    }
+    while (!data.empty() && data.back() == 0) {
+        // Пока вектор не пуст и последний элемент равен нулю
+        data.pop_back();  // удаляем этот нулевой элемент
+    }
     system("pause");
-    return 0;
 }
